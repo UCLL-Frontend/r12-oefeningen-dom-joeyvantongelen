9960ea9a425bb667dd0dfa3039d6147e648131ff
joeyvantongelen <joey.vantongelen@student.ucll.be>
2024-12-11 14:27:56 +0100

    Finished exercises

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ paragraphButton.addEventListener("click", () => {
 const colorButton = document.getElementById("changeColor");
 const colorBox = document.getElementById("colorBox");
 colorButton.addEventListener("click", () => {
-  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16)
-  colorBox.style.background-color = randomColor)
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  colorBox.setAttribute("style", `background-color: ${randomColor}`);
 });
 
 const hideButton = document.getElementById("hideText");
@@ -23,3 +23,23 @@ hideButton.addEventListener("click", () => {
   text.setAttribute("style", "display: none");
 });
 
+const addItemButton = document.getElementById("addItem");
+const list = document.getElementById("itemList");
+addItemButton.addEventListener("click", () => {
+  let newLi = document.createElement("li");
+  newLi.appendChild(document.createTextNode("Nieuw item"));
+  list.appendChild(newLi);
+});
+
+const textColorButton = document.getElementById("changeTextColor");
+const coloredtext = document.getElementById("coloredText");
+textColorButton.addEventListener("click", () => {
+  coloredtext.setAttribute("style", "color: red");
+});
+
+const inputButton = document.getElementById("showInput");
+const userInput = document.getElementById("userInput");
+const userOutput = document.getElementById("output");
+inputButton.addEventListener("click", () => {
+  userOutput.textContent = userInput.value;
+});
